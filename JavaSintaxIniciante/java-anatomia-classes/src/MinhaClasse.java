cdcb195de3678d88674013495a4db72fc6545afb
Constante Binoya <constantebinoyaz1@gmail.com>
2023-02-25 21:25:07 -0300

    Como nomear variáveis

--- JavaSintaxIniciante/java-anatomia-classes/src/MinhaClasse.java
+++ JavaSintaxIniciante/java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,41 @@
-public class MinhaClasse {
+public class MinhaClasse {    /*PADRÃO DE NOMECLATURA:
+                                 1) Arquivo.java:
+                                     Tdod arquivo.java deve começar com letra MAIÚSCULA.
+                                     Se a palavra for composta, a segunda palavra deve, também, ser  MAIÚSCULA.
+                                         Exemplo: Calculadora.java
+                                                  CalculadoraCientifica.java
+                                  
+                                  2) Nome da classse no arquivo: 
+                                      A classe deve possuir o mesmo nome do arquivo.
+                                          Exemplo:  aquivo >>> CalculadoraCientifica.java
+                                                    public class >>> Calculadoracientifica  */
 
 public static void main(String[] args) {
-    System.out.print ( "Olá turma, sejam bem-vindos.");        
-        
-}    
+
+                              /*NOME DE VARIÁVEL:
+                                   Toda variável deve ser escrita com letra minúscula, porém se a palavra for composta, a primeira letra
+                                   da segunda palavra deverá ser MAIÚSCULA.
+                                       Exemplo: ano e anoFabricacao
+                                                O nome dessa prática para nomear variáveis é "camelCase". 
+                                    
+                                    OBS: Existe uma regra adicional para variáveis quando queremos que ela não sofra alterações durante a
+                                         codagem.
+                                             Exemplo: Uma variável com o nome "br" sempre representará "Brasil".*/
+
+    //String BR = "Brasil";
+    //double PI = 3.14;
+    //float PI = 3.14;
+    //int ESTADOS_BRASILEIROS = 27
+    //int ANO = 2023
+
+                              /*RECOMENDAÇÕES PARA NOMEAR UMA VARIÁVEL:
+                                 1) Deve conter apenas letras, _ (UNDERLINE), $ ou números inteiros;
+                                 2) Deve começar SEMPRE por uma letra minúscula;
+                                 3) Não pode conter espaços;
+                                 4) Não pode usar palavras reservadas (palavras-chave);
+                                 5) o nome deve ser único dentro do escopo do código;
+
+                              */
+} 
+
 }
